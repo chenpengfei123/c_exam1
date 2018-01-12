1722c2504c56d6a091c90597cbfb61e31266dc08
pf.chen <1225488780@qq.com>
2018-01-12 17:17:56 +0800

    添加项目文件。

---
+++ 免监考C语言晋级考试系统/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace 免监考C语言晋级考试系统
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ 免监考C语言晋级考试系统/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace 免监考C语言晋级考试系统
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCapture_Click(object sender, RoutedEventArgs e)
+        {
+            CameraHelper.CaptureImage(@"E:\1");
+        }
+
+        private void btnOpenCamera_Click(object sender, RoutedEventArgs e)
+        {
+            CameraHelper.UpdateCameraDevices();
+            if (CameraHelper.CameraDevices.Count > 0)
+            {
+                CameraHelper.SetCameraDevice(0);
+            }
+        }
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            CameraHelper.CloseDevice();
+        }
+    }
+}
